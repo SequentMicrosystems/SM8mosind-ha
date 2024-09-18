d93bb920b4293d5434b5e04e8b2e7c4c3b0079ef
3lv <cristian.vlad.vladutu@gmail.com>
2024-09-18 19:32:29 +0300

    First commit

---
+++ custom_components/SM8mosind/data.py
@@ -0,0 +1,28 @@
+import lib8mosind
+API = lib8mosind
+DOMAIN = "SM8mosind"
+NAME_PREFIX = "sm8m"
+SM_MAP = {
+    "switch": {
+        "mos": {
+                "chan_no": 8,
+                "com": {
+                    "get": "get",
+                    "set": "set"
+                },
+        }
+    },
+    "number": {
+        "pwm": {
+                "chan_no": 8,
+                "uom": "%",
+                "min_value": 0.0,
+                "max_value": 100.0,
+                "step": 0.1,
+                "com": {
+                    "get": "get_pwm",
+                    "set": "set_pwm"
+                },
+        },
+    },
+}
